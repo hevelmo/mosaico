1d03d9dea638dce9c22ac6970550fd476bd53ec4
hevelmo <hevelmo060683@gmail.com>
2020-01-28 18:08:39 -0600

    Initial commit

---
+++ js/core.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+      $('.mosaic-bg-img-holder .mosaic-gallery-img').addClass('fade-In');
+      $('.mosaic-bg-img-holder .mosaic-gallery-img').each(function () {
+            var imgSrc = $(this).children('img').attr('src');
+            var styles = {
+                  'background': 'url("' + imgSrc + '")',
+                  'background-color': '#F9FBFF',
+                  'background-position': '50% 50%',
+                  'background-size': '90% 90%',
+                  'background-repeat': 'no-repeat'
+            };
+            $(this).css(styles);
+            $(this).children('img').hide();
+      });
+});
